892a7ff7a33997ffe7306c65b2fa83c541fe6ac6
Thalles Rafael <thallesrafaell@outlook.com>
2023-11-07 20:07:58 -0300

    exercicio_ts

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
-"use strict";
 //• Utilizando o TypeScript e a tipagem escreva duas funções: 
 //1 - uma função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles
 //2 - uma função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome;
-function baseAltura(base, altura) {
-    return base * altura;
+
+function baseAltura(base: number, altura: number) {
+    return base * altura
 }
-console.log(baseAltura(10, 10));
-function dizOi(nome) {
-    return "Olá " + nome;
+
+console.log(baseAltura(10, 10))
+
+function dizOi(nome: string) {
+    return   "Olá " + nome 
 }
-console.log(dizOi("Thalles"));
+
+console.log(dizOi("Thalles"))
