658f80010f71e721858902fa378d0effd5ff1384
Thalles Rafael <thallesrafaell@outlook.com>
2023-12-03 19:18:35 -0300

    teste cypress

---
+++ cypress/e2e/ListaDeContatos/ListaDeContatos.cy.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+
+describe("Testes para exercicio", () => {
+  beforeEach(() => {
+    cy.visit("https://agenda-contatos-react.vercel.app/");
+  });
+
+  it("Deve renderizar 3 contatos", () => {
+    cy.get(".contato").should("have.length", 3);
+  });
+
+  it("deve adicionar um novo contato", () => {
+    cy.get('[type="text"]').type("Thalles Rafaell");
+    cy.get('[type="email"]').type("thalles@gmail.com");
+    cy.get('[type="tel"]').type("11999999999{enter}");
+
+    cy.get(".contato").should("have.length", 4);
+  });
+
+  it("deve editar e salvar", () => {
+    cy.get(":nth-child(2) > .sc-gueYoa > .edit").click();
+    cy.get('[type="text"]').clear().type("Lucas");
+    cy.get('[type="email"]').clear().type("Lucas@gmail.com");
+    cy.get('[type="tel"]').clear().type("11999889999{enter}");
+    cy.get(".alterar").click();
+
+    cy.get(":nth-child(2) > .sc-dmqHEX > .sc-eDDNvR > :nth-child(1)").should(
+      "have.text",
+      "Lucas"
+    );
+  });
+
+  it("deve remover um contato", () => {
+    cy.get(":nth-child(4) > .sc-gueYoa > .delete").click();
+
+    cy.get(".contato").should("have.length", 3);
+  });
+});
